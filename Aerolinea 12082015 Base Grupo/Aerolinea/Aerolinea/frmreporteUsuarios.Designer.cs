3041c97bc28d9d5d6403a052fa81d3a5114d537d
JGerar1193 <josegerxrdo@gmail.com>
2015-08-30 21:18:26 -0600

    Formulario de Reporte
    Formulario de Reporte
    Muestra un detallado por tipo de Accion
    y un general por total de transacciones

---
+++ Aerolinea 12082015 Base Grupo/Aerolinea/Aerolinea/frmreporteUsuarios.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Aerolinea
+{
+    partial class frmreporteUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmreporteUsuarios));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grdReporte = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sALIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEFRESCARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControlReporte = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.grdReporte2 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdReporte)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.tabControlReporte.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdReporte2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.grdReporte);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 419);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Por Tipo de Transaccion";
+            // 
+            // grdReporte
+            // 
+            this.grdReporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdReporte.Location = new System.Drawing.Point(6, 19);
+            this.grdReporte.Name = "grdReporte";
+            this.grdReporte.Size = new System.Drawing.Size(344, 394);
+            this.grdReporte.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sALIRToolStripMenuItem,
+            this.rEFRESCARToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(425, 27);
+            this.menuStrip1.TabIndex = 184;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sALIRToolStripMenuItem
+            // 
+            this.sALIRToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.sALIRToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sALIRToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("sALIRToolStripMenuItem.Image")));
+            this.sALIRToolStripMenuItem.Name = "sALIRToolStripMenuItem";
+            this.sALIRToolStripMenuItem.Size = new System.Drawing.Size(73, 23);
+            this.sALIRToolStripMenuItem.Text = "SALIR";
+            this.sALIRToolStripMenuItem.Click += new System.EventHandler(this.sALIRToolStripMenuItem_Click);
+            // 
+            // rEFRESCARToolStripMenuItem
+            // 
+            this.rEFRESCARToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("rEFRESCARToolStripMenuItem.Image")));
+            this.rEFRESCARToolStripMenuItem.Name = "rEFRESCARToolStripMenuItem";
+            this.rEFRESCARToolStripMenuItem.Size = new System.Drawing.Size(96, 23);
+            this.rEFRESCARToolStripMenuItem.Text = "REFRESCAR";
+            this.rEFRESCARToolStripMenuItem.Click += new System.EventHandler(this.rEFRESCARToolStripMenuItem_Click);
+            // 
+            // tabControlReporte
+            // 
+            this.tabControlReporte.Controls.Add(this.tabPage1);
+            this.tabControlReporte.Controls.Add(this.tabPage2);
+            this.tabControlReporte.Location = new System.Drawing.Point(20, 30);
+            this.tabControlReporte.Name = "tabControlReporte";
+            this.tabControlReporte.SelectedIndex = 0;
+            this.tabControlReporte.Size = new System.Drawing.Size(382, 457);
+            this.tabControlReporte.TabIndex = 185;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(374, 431);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Por Tipo de Transaccion";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(374, 431);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "General por Usuario";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grdReporte2);
+            this.groupBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.groupBox2.Location = new System.Drawing.Point(55, 9);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(264, 419);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "General";
+            // 
+            // grdReporte2
+            // 
+            this.grdReporte2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdReporte2.Location = new System.Drawing.Point(15, 19);
+            this.grdReporte2.Name = "grdReporte2";
+            this.grdReporte2.Size = new System.Drawing.Size(243, 394);
+            this.grdReporte2.TabIndex = 1;
+            // 
+            // frmreporteUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 499);
+            this.ControlBox = false;
+            this.Controls.Add(this.tabControlReporte);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "frmreporteUsuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reporte Usuarios";
+            this.Load += new System.EventHandler(this.fromreporteUsuarios_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdReporte)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControlReporte.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdReporte2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView grdReporte;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sALIRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEFRESCARToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControlReporte;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView grdReporte2;
+    }
+}
---
+++ Aerolinea 12082015 Base Grupo/Aerolinea/Aerolinea/frmreporteUsuarios.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+//Programador y Analista: Gerardo Santizo
+//Fecha inicio: 29/08/2015  Fecha finalizacion: 30/08/2015
+
+namespace Aerolinea
+{
+    public partial class frmreporteUsuarios : Form
+    {
+        public frmreporteUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void fromreporteUsuarios_Load(object sender, EventArgs e)
+        {
+            funllenarGridReporte();
+            funllenarGridReporteGeneral();
+        }
+        private void funllenarGridReporte()
+        {
+            using (clasconexion.funobtenerConexion())
+            {
+                string squery = "SELECT vuser as NombreUsuario, vaccion as Accion, count(vaccion) as transacciones from TrBITACORA group by vaccion";
+                MySqlCommand cmdc = new MySqlCommand(squery, clasconexion.funobtenerConexion());
+                DataTable dtDatos = new DataTable();
+                MySqlDataAdapter mdaDatos = new MySqlDataAdapter(squery, clasconexion.funobtenerConexion());
+                mdaDatos.Fill(dtDatos);
+                grdReporte.DataSource = dtDatos;
+
+            }
+        }
+        private void funllenarGridReporteGeneral()
+        {
+            using (clasconexion.funobtenerConexion())
+            {
+                string squery = "SELECT vuser as NombreUsuario, count(vaccion) as transacciones from TrBITACORA group by vuser";
+                MySqlCommand cmdc = new MySqlCommand(squery, clasconexion.funobtenerConexion());
+                DataTable dtDatos = new DataTable();
+                MySqlDataAdapter mdaDatos = new MySqlDataAdapter(squery, clasconexion.funobtenerConexion());
+                mdaDatos.Fill(dtDatos);
+                grdReporte2.DataSource = dtDatos;
+            }
+        }
+
+        private void sALIRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void rEFRESCARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            funllenarGridReporte();
+            funllenarGridReporte();
+        }
+
+    }
+}
